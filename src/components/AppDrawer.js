afadb4baddd8880259cf9428350a9b62e6a1b4c1
McTurner45 <58169969+McTurner45@users.noreply.github.com>
2021-03-23 00:06:10 +0200

    added map

---
+++ src/components/AppDrawer.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { mainListItems } from './ListItems';
+// import firebase from '../../firebase'
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Link, withRouter } from 'react-router-dom'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import {styles} from '../css/CustomStyles'
+
+function AppDrawer(props) {
+    const { classes } = props
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+    /*if (!firebase.getCurrentUsername()) {
+        // not logged in
+        alert('Please login first')
+        props.history.replace('/login')
+        return null
+    }*/
+
+    return (
+        <div className="root">
+            <CssBaseline />
+            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        {/*{firebase.getCurrentUsername()}*/}
+                    </Typography>
+                    <IconButton color="inherit">
+                        <Badge badgeContent={4} color="secondary">
+                            <NotificationsIcon />
+                        </Badge>
+                    </IconButton>
+                    <IconButton color="inherit">
+                        <Badge color="secondary"
+                               //onClick={logout}
+                            >
+                            <ExitToAppIcon />
+                        </Badge>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <List>{mainListItems}</List>
+
+            </Drawer>
+        </div>
+    );
+
+
+}
+/*async function logout() {
+        await firebase.logout()
+        props.history.push('/')
+    }*/
+export default withRouter(withStyles(styles)(AppDrawer))
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,28 +1,38 @@
 import React from 'react'
-import { GoogleMap, Marker, withScriptjs, withGoogleMap } from "react-google-maps"
+import {GoogleMap, withGoogleMap, withScriptjs} from "react-google-maps"
+import AppDrawer from "../components/AppDrawer";
+import {CssBaseline} from "@material-ui/core";
+import {styles} from '../css/CustomStyles'
+import {withRouter} from "react-router-dom";
+import withStyles from "@material-ui/core/styles/withStyles";
+
 
 function Map(){
     return(
         <GoogleMap
-            defaultZoom={18}
-            defaultCenter={{lat:-22.328474 , lgn:24.684866}}/>
+            defaultZoom={15}
+            defaultCenter={{lat: -22.556160, lng: 27.130541}}/>
     );
 }
 
 const WrappedMap= withScriptjs(withGoogleMap(Map));
 
-const dashboard = () => {
+const dashboard = (props) => {
+    const { classes } = props
   return (
-    <div className="map-container">
-       <WrappedMap
-           //googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBhdv6C6ZtgNOvTsdmkAAZicXHTqSdqAoU"
-           loadingElement={<div style={{ height: `100%` }} />}
-           containerElement={<div style={{ height: `100%` }} />}
-           mapElement={<div style={{ height: `100%` }} />}
-       />
-    </div>
+      <div className="root">
+          <CssBaseline/>
+          <AppDrawer/>
+          <div className="map-container">
+              <WrappedMap
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBhdv6C6ZtgNOvTsdmkAAZicXHTqSdqAoU"
+                  loadingElement={<div style={{height: `100%`}}/>}
+                  containerElement={<div style={{height: `100%`}}/>}
+                  mapElement={<div style={{height: `100%`}}/>}
+              />
+          </div>
+      </div>
   )
 }
 
-export default dashboard
+export default withRouter(withStyles(styles)(dashboard))
